be8011198a1fae8873cb447451440f3624ad4371
HelgaN <ola14@tut.by>
2018-03-27 17:03:33 +0300

    Правки по переполнению контентом

--- js/map.js
+++ js/map.js
@@ -3,17 +3,17 @@ var mapLink = document.querySelector(".button-contacts-map");
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-map-close");
 
-mapLink.addEventListener("click", function (e) {
+mapLink.addEventListener("click", function(e) {
   e.preventDefault();
   mapPopup.classList.add("modal-show");
 });
 
-mapClose.addEventListener("click", function (e) {
+mapClose.addEventListener("click", function(e) {
   e.preventDefault();
   mapPopup.classList.remove("modal-show");
 });
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener("keydown", function(e) {
   e.preventDefault();
   if (e.keyCode === 27) {
     if (mapPopup.classList.contains("modal-show")) {
